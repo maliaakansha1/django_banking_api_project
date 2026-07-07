fe01804123db9cc1ee3572daa079f11b5ae2ff60
Aakansha <mali.aakansha1@gmail.com>
2026-07-07 19:10:50 +0530

    Update project settings

--- banking_api/settings.py
+++ banking_api/settings.py
@@ -80,11 +80,14 @@ WSGI_APPLICATION = 'banking_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'banking_db',
+        'USER': 'postgres',
+        'PASSWORD': 'aakansha01',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
